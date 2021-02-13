87c4af95c08c565ef513feb7c045ba81636a3e87
P0645300 <rodrigo.magalhaes@portoseguro.com.br>
2021-02-13 16:10:26 -0300

    Registration people with Flutter

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(id: '1', name: 'Joao', email: 'Joao@joao.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'),
+  '2': const User(id: '2', name: 'Francisca', email: 'francisca@joao.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/22/09/18/woman-5939537_960_720.jpg'),
+  '3': const User(id: '3', name: 'Fernanda', email: 'fernanda@joao.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/24/09/28/girl-5944690_960_720.png'),
+  '4': const User(id: '4', name: 'Marcela', email: 'marcela@joao.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2021/01/29/04/52/character-5959683_960_720.png'),
+  '5': const User(id: '5', name: 'Mario', email: 'mario@joao.com.br', avatarUrl: 'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_960_720.png'),
+};
